eba6c193eafa1126e8acbc346034d4d81ec83062
arsen056 <61593202+arsen056@users.noreply.github.com>
2021-03-14 20:20:12 +0300

    Test layout. Scandi. Marathon 3.0

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    // Slick slider
+    $('.slider-reviews').slick({
+        infinite: false,
+    });
+    
+    // Ipnut mask
+    $("#input_number").inputmask({ "mask": "+7 (999) 999-99-99" });
+    
+    // Smooth transition
+    $("#nav").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+
+});
+
